81f169a04db4dcfd0bccebb3e25f22b2a2b981e0
arfan-rfn <arfan.rfn@gmail.com>
2025-02-01 15:45:35 -0700

    added initial setup for the database

--- chapter2/licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ chapter2/licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -2,8 +2,12 @@ package com.optimagrowth.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.optimagrowth.license.model")
+@EnableJpaRepositories("com.optimagrowth.license.repository")
 public class LicenseServiceApplication {
 
 	public static void main(String[] args) {
